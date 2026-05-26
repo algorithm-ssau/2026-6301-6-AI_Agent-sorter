9a8259adae47b89ac804e61851a28e4bd7cb5410
Timofei Cherevko <cerevkotimofej4@gmail.com>
2026-05-26 11:17:00 +0400

    Add app module imports and path constants

---
+++ app.py
@@ -0,0 +1,8 @@
+import time
+
+from brain import GeminiBrain as NvidiaBrain
+from io_handler import FileHandler
+from schema import AgentPrompts
+
+INPUT_PATH = "./data/input"
+OUTPUT_PATH = "./data/output"
